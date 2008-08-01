5d536f5cb18bde12b1d89a3330ea1525b85a8cca
cmsbuild <>
2008-08-01 12:03:19 +0000

    This commit was manufactured by cvs2git to create tag 'RecoMuon- L2MuonProducer-CMSSW_2_2_0'.
    Sprout from master 2008-06-19 14:55:23 UTC Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch> 'removed inclusion of now obsolete MagneticField/Engine/data/volumeBasedMagneticField.cfi'
    Cherrypick from master 2008-08-01 12:03:03 UTC Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch> 'remove spurious import of Geomtry.CMSCommonData.cmsIdealGeometryXML.cff that should not be done insided reco configurations and only in the top level cfg':
        python/L2Muons_cff.py

--- python/L2Muons_cff.py
+++ python/L2Muons_cff.py
@@ -2,11 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 # Magnetic Field
 # Geometries
-from Geometry.CMSCommonData.cmsIdealGeometryXML_cff import *
 from Geometry.CommonDetUnit.bareGlobalTrackingGeometry_cfi import *
-from Geometry.DTGeometry.dtGeometry_cfi import *
-from Geometry.CSCGeometry.cscGeometry_cfi import *
-from Geometry.RPCGeometry.rpcGeometry_cfi import *
 from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
 import TrackingTools.KalmanUpdators.Chi2MeasurementEstimatorESProducer_cfi
 EstimatorForSTA = TrackingTools.KalmanUpdators.Chi2MeasurementEstimatorESProducer_cfi.Chi2MeasurementEstimator.clone()
